04e2f3618f725d6987df8e3d48d9176e653bf00d
Jam3e3333 <jamm3e3333@gmail.com>
2021-07-29 18:57:22 -0400

    css for containers

--- monitor-parking/src/App.js
+++ monitor-parking/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import classes from './App.module.css';
+import Card from './components/UI/Card';
+import ParkingInfoParagraph from './components/ParkingInfo/ParkingInfo/ParkingInfoParagraph';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={classes['header__main']}>
+        <Card>
+          <p>ahoj</p>
+        </Card>
+        <Card>
+          <ParkingInfoParagraph 
+            title="Parkovaci doba"
+            data={new Date().toISOString()}
+          />
+        </Card>
+      </div>
   );
 }
 
---
+++ monitor-parking/src/components/ParkingInfo/ParkingInfo/ParkingInfoParagraph.js
@@ -0,0 +1,12 @@
+import classes from './ParkingInfoParagraph.module.css';
+
+const ParkingInfoParagraph = props => {
+    return (
+        <p className={classes['parking__info--paragraph']}>
+            <span className={classes['parking__info--item']}>{props.title}</span>
+            <span className={classes['parking__info--item']}>{props.data}</span>
+        </p>
+    )
+}
+
+export default ParkingInfoParagraph;
