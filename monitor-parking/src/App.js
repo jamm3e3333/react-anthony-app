6d981eadebf12903b0f3940188a3a1e19c08c487
Jam3e3333 <jamm3e3333@gmail.com>
2021-08-02 12:43:35 -0400

    parking space visualization

--- monitor-parking/src/App.js
+++ monitor-parking/src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-
 import classes from './App.module.css';
-
-import MapImg from './assets/map__view.png';
 
 import Card from './components/UI/Card';
 import ParkingCard from './components/ParkingInfoComponents/ParkingCard'
-import CardItem from './components/UI/CardItem'
 import ParkingTiming from './components/ParkingInfoComponents/ParkingTiming/ParkingTiming';
+import ParkingMap from './components/ParkingInfoComponents/ParkingMap';
 
 
 const App = () => {
@@ -16,91 +12,198 @@ const App = () => {
     parkingLots: [
     {
       id: 'lot0',
-      occupated: true,
-      parkingStart: 627643300000,
+      occupated: false,
+      parkingStart: 0,
+      coord: {
+        x: 10,
+        y: 150
+      }
     },
     {
       id: 'lot1',
       occupated: true,
       parkingStart: 1627646500000,
+      coord: {
+        x: 30,
+        y: 170
+      }
     },
     {
       id: 'lot2',
-      occupated: true,
-      parkingStart: 1627640005000,
+      occupated: false,
+      parkingStart: 0,
+      coord: {
+        x: 50,
+        y: 190
+      }
     },
     {
       id: 'lot3',
       occupated: true,
       parkingStart: 1627640075000,
+      coord: {
+        x: 90,
+        y: 210
+      }
     },
     {
       id: 'lot4',
       occupated: false,
       parkingStart: 0,
+      coord: {
+        x: 120,
+        y: 190
+      }
     },
     {
       id: 'lot5',
       occupated: false,
       parkingStart: 0,
+      coord: {
+        x: 150,
+        y: 170
+      }
     },
     {
       id: 'lot6',
       occupated: true,
       parkingStart: 1627640780000,
+      coord: {
+        x: 180,
+        y: 150
+      }
     },
     {
       id: 'lot7',
       occupated: false,
       parkingStart: 0,
+      coord: {
+        x: 210,
+        y: 130
+      }
     },
     {
       id: 'lot8',
       occupated: true,
       parkingStart: 1627640870000,
+      coord: {
+        x: 240,
+        y: 110
+      }
     },
     {
       id: 'lot9',
       occupated: false,
       parkingStart: 0,
+      coord: {
+        x: 270,
+        y: 90
+      }
     },
     {
       id: 'lot10',
       occupated: true,
       parkingStart: 162764060000,
+      coord: {
+        x: 180,
+        y: 260
+      }
     },
     {
       id: 'lot11',
       occupated: false,
       parkingStart: 0,
+      coord: {
+        x: 210,
+        y: 240
+      }
     },
     {
       id: 'lot12',
       occupated: false,
       parkingStart: 0,
+      coord: {
+        x: 240,
+        y: 220
+      }
     },
     {
       id: 'lot13',
       occupated: true,
       parkingStart: 1627646500000,
+      coord: {
+        x: 270,
+        y: 200
+      }
     },
     {
       id: 'lot14',
       occupated: true,
       parkingStart: 1627640540000,
+      coord: {
+        x: 300,
+        y: 180
+      }
     },
     {
       id: 'lot15',
       occupated: true,
       parkingStart: 1620000000000,
+      coord: {
+        x: 330,
+        y: 160
+      }
     },
+    {
+      id: 'lot16',
+      occupated: true,
+      parkingStart: 1620000000000,
+      coord: {
+        x: 240,
+        y: 270
+      }
+    },
+    {
+      id: 'lot17',
+      occupated: true,
+      parkingStart: 1620000000000,
+      coord: {
+        x: 270,
+        y: 250
+      }
+    },
+    {
+      id: 'lot18',
+      occupated: true,
+      parkingStart: 1620000000000,
+      coord: {
+        x: 300,
+        y: 230
+      }
+    },
+    {
+      id: 'lot19',
+      occupated: true,
+      parkingStart: 1620000000000,
+      coord: {
+        x: 330,
+        y: 210
+      }
+    },
+    {
+      id: 'lot20',
+      occupated: true,
+      parkingStart: 1620000000000,
+      coord: {
+        x: 360,
+        y: 190
+      }
+    }
   ]}
   return (
       <div className={classes['header__main']}>
         <Card>
-          <CardItem>
-            <img src={MapImg} alt="map view" />
-          </CardItem>
+          <ParkingMap items={parkingInfo} />
         </Card>
         <Card>
           <ParkingCard items={parkingInfo} />
---
+++ monitor-parking/src/components/ParkingInfoComponents/ParkingMap.js
@@ -0,0 +1,28 @@
+import { Stage, Layer, Circle } from 'react-konva';
+
+import CardItem from '../UI/CardItem';
+import classesCardItem from '../UI/CardItem.module.css';
+
+const ParkingMap = props => {
+    return (
+        <CardItem className={classesCardItem['card__item--map']}>
+          <Stage width={640} height={400} >
+            <Layer>
+                {props.items.parkingLots.map(item => {
+                  return (
+                    <Circle 
+                      key={item.id} 
+                      x={item.coord.x} 
+                      y={item.coord.y} 
+                      radius={8} 
+                      fill={item.occupated ? "red" : "green"} 
+                    />
+                  )
+                })}
+            </Layer>  
+          </Stage>  
+        </CardItem>
+    )
+}
+
+export default ParkingMap;
